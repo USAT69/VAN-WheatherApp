b49e089a97ad983fb5d7ab473ad1ec7ec5d111f4
Anshul thakur <ushathakur1441@gmail.com>
2026-05-14 14:40:19 +0530

    Update Van.js

--- Van.js
+++ Van.js
@@ -30,7 +30,7 @@ async function weather(city){
 
       cc.style.padding =
       "20px 5px";
-      cc.style.marginTop="10vh";
+      cc.style.marginTop="10px";
 
 
      if(dt.weather[0].main=="Rain"){
